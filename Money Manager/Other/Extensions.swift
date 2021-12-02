683b4be0e9d94c50a8eaf71b853331dd9f62f8b3
falalieiev <falalieievoleh@gmail.com>
2021-12-02 20:30:04 +0200

    Add date formatter

--- Money Manager/Other/Extensions.swift
+++ Money Manager/Other/Extensions.swift
@@ -45,3 +45,10 @@ extension HorizontalBarChartView {
     }
 }
 
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
+   }
+}
