5b5dd80494cfa9bc4189ab0f9bede1a75167ce65
falalieiev <falalieievoleh@gmail.com>
2021-12-02 20:29:26 +0200

    Transaction now sorted by date

--- Money Manager/Controllers/MainVC.swift
+++ Money Manager/Controllers/MainVC.swift
@@ -24,8 +24,11 @@ class MainVC: UIViewController, MainVCDelegate {
     var billIndex: Int?
     var sumOfIncome: Float = 0.0
     var sumOfExpenses: Float = 0.0
-    var billValue1: Float = 0.0
+    var billValueFloat: Float = 0.0
     var billSymbol = ""
+    var groupedItems = [Date:Results<Transaction>]()
+    var itemDates = [Date]()
+    var items: Results<Transaction>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,18 +44,27 @@ class MainVC: UIViewController, MainVCDelegate {
         tableView.delegate = self
         tableView.dataSource = self
         
-        if bill.count != 0 {
-            billIndex = 0
-        }
-        
         loadTransactions()
+        
+        tableView.sectionHeaderTopPadding = 0
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        if billIndex == nil {
+            billIndex = 0
+        }
+        
         if bill.isEmpty == false {
             loadTransactions()
             configureBill(with: bill[billIndex ?? 0])
             loadTransactions()
+        } else {
+            billImage.image = UIImage(systemName: "plus")
+            billValue.text = ""
+            billName.text = "Добавить счет"
+            expensesLabel.text = "0.0$"
+            incomeLabel.text = "0.0$"
+            tableView.reloadData()
         }
     }
     
@@ -65,13 +77,15 @@ class MainVC: UIViewController, MainVCDelegate {
     }
     
     @objc func summaryTapped(sender : UITapGestureRecognizer) {
+        if bill.count != 0 {
             performSegue(withIdentifier: "transactionObserve", sender: sender)
+        }
     }
     
     func configureBill(with bill: Bill) {
         billImage.image = UIImage(systemName: "creditcard")
         billName.text = bill.name
-        billValue.text = String(bill.budget + billValue1) + bill.currency
+        billValue.text = String(bill.budget + billValueFloat) + bill.currency
         billSymbol = bill.currency
     }
     
@@ -103,12 +117,33 @@ class MainVC: UIViewController, MainVCDelegate {
     func loadTransactions() {
         if billIndex != nil {
             transactionObject = bill[billIndex ?? 0].transaction
+            
+            items = transactionObject?.sorted(byKeyPath: "creationDate", ascending: false)
+            
+            itemDates = items!.reduce(into: [Date](), { results, currentItem in
+                let date = currentItem.creationDate!
+                let beginningOfDay = Calendar.current.date(from: DateComponents(year: Calendar.current.component(.year, from: date), month: Calendar.current.component(.month, from: date), day: Calendar.current.component(.day, from: date), hour: 0, minute: 0, second: 0))!
+                let endOfDay = Calendar.current.date(from: DateComponents(year: Calendar.current.component(.year, from: date), month: Calendar.current.component(.month, from: date), day: Calendar.current.component(.day, from: date), hour: 23, minute: 59, second: 59))!
+                
+                if !results.contains(where: { addedDate->Bool in
+                    return addedDate >= beginningOfDay && addedDate <= endOfDay
+                }) {
+                    results.append(beginningOfDay)
+                }
+            })
+          
+            groupedItems = itemDates.reduce(into: [Date:Results<Transaction>](), { results, date in
+                let beginningOfDay = Calendar.current.date(from: DateComponents(year: Calendar.current.component(.year, from: date), month: Calendar.current.component(.month, from: date), day: Calendar.current.component(.day, from: date), hour: 0, minute: 0, second: 0))!
+                let endOfDay = Calendar.current.date(from: DateComponents(year: Calendar.current.component(.year, from: date), month: Calendar.current.component(.month, from: date), day: Calendar.current.component(.day, from: date), hour: 23, minute: 59, second: 59))!
+                results[beginningOfDay] = items!.filter("creationDate >= %@ AND creationDate <= %@", beginningOfDay, endOfDay)
+            })
+            
             sumOfIncome = RealmService.shared.sumOfIncome(object: transactionObject)
             sumOfExpenses = RealmService.shared.sumOfExpenses(object: transactionObject)
             expensesLabel.text = sumOfExpenses.floatToString(sumOfExpenses) + billSymbol
             incomeLabel.text = sumOfIncome.floatToString(sumOfIncome) + billSymbol
-
-            billValue1 = sumOfIncome - sumOfExpenses
+            billValueFloat = sumOfIncome - sumOfExpenses
+            configureBill(with: bill[billIndex ?? 0])
             tableView.reloadData()
         }
     }
@@ -120,30 +155,58 @@ extension MainVC: UITableViewDelegate {
         
         print(currentCell.transactionName.text ?? "null")
     }
+    
 }
 
 extension MainVC: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "transaction", for: indexPath) as! TransactionCell
-        if let transaction = transactionObject?[indexPath.row] {
-            cell.transactionName.text = transaction.category
-            cell.transactionValue.text = String(transaction.value) + billSymbol
-            if transaction.transactionType == 0 {
-                cell.transactionType.tintColor = .red
-                cell.transactionType.image = UIImage(systemName: "arrow.down")
-            } else {
-                cell.transactionType.tintColor = #colorLiteral(red: 0.2045667768, green: 0.7816080451, blue: 0.3486227691, alpha: 1)
-                cell.transactionType.image = UIImage(systemName: "arrow.up")
-            }
-        }
-        return cell
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let trashAction = UIContextualAction(style: .destructive, title:  "Trash", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
+            RealmService.shared.delete(self.items![indexPath.row])
+            self.loadTransactions()
+            success(true)
+        })
+        trashAction.backgroundColor = .red
+        trashAction.image = UIImage(systemName: "trash")
+        
+        let editAction = UIContextualAction(style: .normal, title:  "More", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
+            print("Update action 2...")
+            success(true)
+        })
+        editAction.backgroundColor = .gray
+        editAction.image = UIImage(systemName: "square.and.pencil")
+        
+        return UISwipeActionsConfiguration(actions: [trashAction, editAction])
+    }
+    
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return itemDates.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let formate = itemDates[section].getFormattedDate(format: "yyyy-MM-dd")
+        return itemDates[section].getFormattedDate(format: formate)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if billIndex != nil {
-            return bill[billIndex ?? 0].transaction.count
+        return groupedItems[itemDates[section]]!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "transaction", for: indexPath) as! TransactionCell
+        let itemsForDate = groupedItems[itemDates[indexPath.section]]!
+         cell.transactionName.text = "\(Array(itemsForDate)[indexPath.row].category)"
+        cell.transactionValue.text = "\(Array(itemsForDate)[indexPath.row].value)"
+        if Array(itemsForDate)[indexPath.row].transactionType == 0 {
+            cell.transactionType.tintColor = .red
+            cell.transactionType.image = UIImage(systemName: "arrow.down")
+        } else {
+            cell.transactionType.tintColor = #colorLiteral(red: 0.2045667768, green: 0.7816080451, blue: 0.3486227691, alpha: 1)
+            cell.transactionType.image = UIImage(systemName: "arrow.up")
         }
-        return 0
+        return cell
     }
 }
-
