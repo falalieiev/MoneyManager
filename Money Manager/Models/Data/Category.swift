d4bb3d3cadcbcd830e56e2548f87f1693304bf0e
falalieiev <falalieievoleh@gmail.com>
2021-12-05 20:23:46 +0200

    Add date formatter

---
+++ Money Manager/Models/Data/Category.swift
@@ -0,0 +1,8 @@
+//
+//  Category.swift
+//  Money Manager
+//
+//  Created by Oleh Falalieiev on 05.12.2021.
+//
+
+import Foundation
--- Money Manager/Other/Extensions.swift
+++ Money Manager/Other/Extensions.swift
@@ -20,35 +20,35 @@ extension Float {
 
 extension HorizontalBarChartView {
     func chartSetting(_ chartView: HorizontalBarChartView) {
-    chartView.drawBarShadowEnabled = false
-    chartView.drawValueAboveBarEnabled = true
-    
-    let xAxis = chartView.xAxis
-    xAxis.labelPosition = .bottom
-    xAxis.labelFont = .systemFont(ofSize: 10)
-    xAxis.drawAxisLineEnabled = true
-    xAxis.granularity = 10
-    
-    let leftAxis = chartView.leftAxis
-    leftAxis.labelFont = .systemFont(ofSize: 10)
-    leftAxis.drawAxisLineEnabled = true
-    leftAxis.drawGridLinesEnabled = true
-    leftAxis.axisMinimum = 0
-    
-    let rightAxis = chartView.rightAxis
-    rightAxis.enabled = true
-    rightAxis.labelFont = .systemFont(ofSize: 10)
-    rightAxis.drawAxisLineEnabled = true
-    rightAxis.axisMinimum = 0
-    
-    chartView.fitBars = true
+        chartView.drawBarShadowEnabled = false
+        chartView.drawValueAboveBarEnabled = true
+        
+        let xAxis = chartView.xAxis
+        xAxis.labelPosition = .bottom
+        xAxis.labelFont = .systemFont(ofSize: 10)
+        xAxis.drawAxisLineEnabled = true
+        xAxis.granularity = 10
+        
+        let leftAxis = chartView.leftAxis
+        leftAxis.labelFont = .systemFont(ofSize: 10)
+        leftAxis.drawAxisLineEnabled = true
+        leftAxis.drawGridLinesEnabled = true
+        leftAxis.axisMinimum = 0
+        
+        let rightAxis = chartView.rightAxis
+        rightAxis.enabled = true
+        rightAxis.labelFont = .systemFont(ofSize: 10)
+        rightAxis.drawAxisLineEnabled = true
+        rightAxis.axisMinimum = 0
+        
+        chartView.fitBars = true
     }
 }
 
 extension Date {
-   func getFormattedDate(format: String) -> String {
+    func getFormattedDate(format: String) -> String {
         let dateformat = DateFormatter()
         dateformat.dateFormat = format
         return dateformat.string(from: self)
-   }
+    }
 }
