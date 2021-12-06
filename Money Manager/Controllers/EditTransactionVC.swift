e774004932c16d6edbb80b8f6795e1877cfac619
falalieiev <falalieievoleh@gmail.com>
2021-12-06 21:00:00 +0200

    Initial Commit

--- Money Manager/Controllers/EditTransactionVC.swift
+++ Money Manager/Controllers/EditTransactionVC.swift
@@ -6,84 +6,131 @@
 //
 
 import UIKit
+import RealmSwift
 
-class EditTransactionVC: UITableViewController {
-
+class EditTransactionVC: UITableViewController, CategoriesDelegate {
+    
+    @IBOutlet weak var note: UILabel!
+    @IBOutlet weak var value: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet var numberPadView: UIView!
+    @IBOutlet weak var cell1: UITableViewCell!
+    @IBOutlet weak var cell2: UITableViewCell!
+    @IBOutlet weak var cell3: UITableViewCell!
+    @IBOutlet weak var barItem: UIBarButtonItem!
+    
+    var transactionForEdit: Transaction!
+    let numberPadManager = NumberPadManager()
+    var categoryInfo: Category?
+    let empty = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        if transactionForEdit.note == "" {
+            note.text = "-"
+        } else {
+            note.text = transactionForEdit.note
+        }
+        value.text = String(transactionForEdit.value)
+        categoryLabel.text = transactionForEdit.category
+        tableView.allowsSelection = false
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.view.addSubview(numberPadView)
+        numberPadView.frame = CGRect(x: 0,
+                                     y: self.view.bounds.size.height - (numberPadView.bounds.size.height + 40),
+                                     width: self.view.bounds.size.width,
+                                     height: numberPadView.bounds.size.height)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        numberPadView.removeFromSuperview()
+    }
+    
+    @IBAction func removeButton(_ sender: UIButton) {
+        numberPadManager.remove(value, sender)
+    }
+    @IBAction func numbers(_ sender: UIButton) {
+        numberPadManager.numbers(value, sender)
+    }
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let currentCell = tableView.cellForRow(at: indexPath)
+        
+        if indexPath.row == 0 {
+            numberPadView.isHidden = true
+            currentCell?.isSelected = false
+            performSegue(withIdentifier: "categories", sender: self)
+        }
+        
+        if indexPath.row == 1 {
+            if numberPadView.isHidden {
+                numberPadView.isHidden = false
+            } else {
+                numberPadView.isHidden = true
+            }
+            currentCell?.isSelected = false
+        }
+        
+        if indexPath.row == 2 {
+            numberPadView.isHidden = true
+            currentCell?.isSelected = false
+            var textField = UITextField()
+            let alert = UIAlertController(title: "Заметка", message: "", preferredStyle: .alert)
+            let change = UIAlertAction(title: "Изменить", style: .default) { (change) in
+                self.note.text = textField.text
+            }
+            let cancel = UIAlertAction(title: "Отменить", style: .cancel, handler: nil)
+            
+            alert.addAction(cancel)
+            alert.addAction(change)
+            alert.addTextField { (field) in
+                textField = field
+                textField.placeholder = "Заметка"
+            }
+            present(alert, animated: true, completion: nil)
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "categories" {
+            let destinationVC = segue.destination as! CategoriesVC
+            destinationVC.delegate = self
+        }
     }
-    */
-
+    
+    func getCategoryInfo(category: Category) {
+        categoryInfo = category
+        categoryLabel.text = category.name
+    }
+    
+    @IBAction func editButton(_ sender: UIBarButtonItem) {
+        if sender.title == "Ред." {
+            sender.title = "Сохранить"
+            cell1.accessoryType = .disclosureIndicator
+            cell2.accessoryType = .disclosureIndicator
+            cell3.accessoryType = .disclosureIndicator
+            tableView.allowsSelection = true
+        }
+        
+        else {
+            let dict: [String: Any?] = ["value": Float(value.text ?? "0.0"),
+                                        "note": note.text,
+                                        "transactionType": categoryInfo?.type ?? transactionForEdit.transactionType,
+                                        "category": categoryInfo?.name ?? transactionForEdit.category]
+            RealmService.shared.update(transactionForEdit, with: dict)
+            cell1.accessoryType = .none
+            cell2.accessoryType = .none
+            cell3.accessoryType = .none
+            tableView.allowsSelection = false
+            //sender.title = "Ред."
+            //tableView.reloadData()
+            navigationController?.popViewController(animated: true)
+        }
+    }
 }
