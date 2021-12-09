40c0c3848bbc27d6d1bed8f91ac0c9f440934c08
falalieiev <falalieievoleh@gmail.com>
2021-12-09 22:30:11 +0200

    Dark mode added

--- Money Manager/Models/Data/UserDefaultsModel.swift
+++ Money Manager/Models/Data/UserDefaultsModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct UserDefaultsModel {
+    static var shared  = UserDefaultsModel()
+    
+    var theme: Theme {
+        get {
+            return Theme(rawValue: UserDefaults.standard.integer(forKey: "selectedTheme")) ?? .light
+        }
+        set {
+            UserDefaults.standard.set(newValue.rawValue, forKey: "selectedTheme")
+        }
+        
+    }
+}
